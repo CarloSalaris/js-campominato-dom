1fb944b09033cfe75222be37a428bb5488234e5b
CarloSalaris <carloa.salaris@gmail.com>
2023-05-10 14:18:23 +0200

    Punto 1 generazione array di 16 numeri casuali tra 1 e 100

--- js/main.js
+++ js/main.js
@@ -1,20 +1,58 @@
 /***************************
     CONSEGNA:
 
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. (NO RIPETIZIONI)
+1) Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. (NO RIPETIZIONI)
 
     Ciclo per creare array (lunghezza = 16) di numeri RANDOM compresi tra 1 e maxNum che non si ripetano
 
-In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+2) In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
 
 Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle. (punti ++)
 
-La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+3) La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
     SE clicca sulla bomba O raggiunge punteggio massimo (maxNum - 16) --> Fine del gioco
 
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 ***************************/
+
+/* SVOLGIMENTO */
+
+//1)
+
+//Genera numero random
+function randomNumMax(numMax) {
+    return Math.floor(Math.random() * numMax) + 1
+}
+// console.log("A random number between 1 and 100: " + randomNumMax(100));
+
+//ARRAY di numeri casuali in un range definito
+function createRandomNumMaxArr(numMax, arrLengthMax) {
+    const intArr = [];
+
+    //Popola array con (arrLengthMax) numeri random tra 1 e numMax
+    while (intArr.length < arrLengthMax) {
+        const newNum = randomNumMax(numMax);
+        if (!intArr.includes(newNum)) {
+            intArr.push(newNum)
+        }
+    }
+    
+    /* for (let i = 0; i < arrLengthMax; i++) {
+        const newNum = randomNumMax(numMax);
+        if (!intArr.includes(newNum)) {
+            intArr.push(newNum)
+        }
+    } */
+    // PERCHÉ FOR LOOP OUTPUTS RANDOM ARRAY.LENGHT?
+
+    return intArr;
+}
+
+
+//2)
+
+
 
 
 // Dichiaro elemento GRIGLIA e elemento BUTTON
@@ -22,19 +60,19 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
     const startButton = document.getElementById("startGame");
 
     // Assegno evento "click" al Button per fare apparire la griglia a seconda dei livelli di difficoltà.
-    startButton.addEventListener("click", 
-        
+    startButton.addEventListener("click",
+
         function() {
 
             //Valore relativo al livello di difficoltà
             const diffLevel = document.getElementById("diffLevel").value;
             // console.log(diffLevel);
-            
+
             // Stabilisco output a seconda del livello
             if (diffLevel == 2){
                 startClickFunction(81, "calc(100% / 9)");
                 console.log("Hai selezionato il livello medio");
-                              
+
             }else if (diffLevel == 3) {
                 startClickFunction(49, "calc(100% / 7)");
                 console.log("Hai selezionato il livello difficile");
@@ -50,6 +88,44 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 /***************************
     FUNCTIONS
 ***************************/
+
+// Funzione PROCESSO PRINCIPALE di click
+function startClickFunction(maxNum, elementWidth) {
+
+    //Svuoto la griglia
+    gridElement.innerHTML = "";
+    //Mostra griglia nascosta
+    showHiddenElement(gridElement);
+    let bombsArray = createRandomNumMaxArr(100, 16)
+    console.log(bombsArray);
+
+    //Creo un loop che si ripeta tante volte quante il valore massimo prestabilito
+    for (let i = 1; i <= maxNum; i++) {
+
+        //Creo una funzione per riprodurre un quadrato (ogni volta)
+        const newSquare = createGridSquare("div", "square", elementWidth);
+
+        //"appendo" il quadrato
+        gridElement.append(newSquare);
+
+        //Creo lo span che andrà in ogni quadrato
+        const newSpan = document.createElement("span");
+        //Lo appendo con il valore "i" del for loop
+        newSpan.innerHTML = i;
+        newSquare.append(newSpan);
+
+        //associo l'evento click a ogni nuovo quadrato creato
+        newSquare.addEventListener("click",
+            function() {
+                // la cella cliccata si colora di azzurro
+                this.classList.add("blue_bg");
+                // emetto un messaggio in console con il numero della cella cliccata.
+                console.log("the number you selected is: " + i)
+            }
+        );
+
+    }
+};
 
 // Funzione per far apparire un elemento non visibile
 function showHiddenElement(elementName) {
@@ -67,46 +143,9 @@ function createGridSquare(tagType, classToAdd, elementWidth) {
     return newElement;
 };
 
-// Funzione PROCESSO PRINCIPALE di click
-function startClickFunction(maxNum, elementWidth) {
-    
-    //Svuoto la griglia
-    gridElement.innerHTML = "";
-    //Mostra griglia nascosta
-    showHiddenElement(gridElement);
-    //Creo un loop che si ripeta tante volte quante il valore massimo prestabilito
-    for (let i = 1; i <= maxNum; i++) {
-        
-        //Creo una funzione per riprodurre un quadrato (ogni volta)
-        const newSquare = createGridSquare("div", "square", elementWidth);
-
-        //"appendo" il quadrato
-        gridElement.append(newSquare);
-        
-        //Creo lo span che andrà in ogni quadrato
-        const newSpan = document.createElement("span");
-        //Lo appendo con il valore "i" del for loop
-        newSpan.innerHTML = i;
-        newSquare.append(newSpan);
-
-        //associo l'evento click a ogni nuovo quadrato creato
-        newSquare.addEventListener("click",
-            function() {
-                // la cella cliccata si colora di azzurro
-                this.classList.add("blue_bg");
-                // emetto un messaggio in console con il numero della cella cliccata.
-                console.log("the number you selected is: " + i)
-            }
-        );
-
-    }           
-};
-
-
 /***************************
     REFACTORING NOTES
 ***************************/
 
 // elementWidth = "calc(100% / Math.sqrt(maxNum))";
     //COME GENERALIZZARLO ALL'INTERNO DELLA FUNZIONE "startClickFunction" PER NON DOVER INSERIRLO OGNI VOLTA?
-                
